661d8f47c7efe7badcc860a1edb34f2feb12ab87
dlwndms0812 <dlwndms0812@naver.com>
2021-02-09 01:03:14 +0900

    영화 페이지 구현

--- app/src/main/java/com/example/guru_group7/recommend/RainyMovie.kt
+++ app/src/main/java/com/example/guru_group7/recommend/RainyMovie.kt
@@ -1,30 +1,12 @@
 package com.example.guru_group7.recommend
 
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.example.guru_group7.R
 
 class RainyMovie: AppCompatActivity() {
-    override fun OnCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.rainy_movie)
-        val items= mutableListOf<ListViewItem>()
-
-        items.add(ListViewItem(ContextCompat.getDrawable(this, R.drawable.rain_meet)!!, "지금, 만나러 갑니다", "감독-도이 노부히로   장르-멜로/로맨스, 드라마, 판타지"))
-        items.add(ListViewItem(ContextCompat.getDrawable(this, R.drawable.rain_notebook)!!, "노트북", "감독-닉 카사베츠   장르-멜로/로맨스, 드라마"))
-        items.add(ListViewItem(ContextCompat.getDrawable(this, R.drawable.rain_classic)!!, "클래식", "감독-곽재용  장르-멜로/로맨스, 드라마"))
-        items.add(ListViewItem(ContextCompat.getDrawable(this, R.drawable.rain_memory)!!, "살인의 추억", "감독-봉준호  장르-범죄, 미스터리, 스릴러, 코미디, 드라마"))
-        items.add(ListViewItem(ContextCompat.getDrawable(this, R.drawable.rain_conjuring)!!, "컨저링", "감독-제임스 완   장르-공포 스릴러"))
-
-        val adapter: ListViewAdapter(items)
-        listView.adpater=adapter
-
-        listView.setOnItemClickListener { parent: AdapterView<*>, view: View, position: Int, id: Long ->
-            val item = parent.getItemAtPosition(position) as ListViewItem
-            Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/guru_group7/recommend/SnowMovie.kt
+++ app/src/main/java/com/example/guru_group7/recommend/SnowMovie.kt
@@ -1,30 +1,12 @@
 package com.example.guru_group7.recommend
 
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.example.guru_group7.R
 
 class SnowMovie: AppCompatActivity() {
-    override fun OnCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.snow_movie)
-        val items= mutableListOf<ListViewItem>()
-
-        items.add(ListViewItem(ContextCompat.getDrawable(this, R.drawable.snow_etener)!!, "이터널 션샤인", "감독-미셸 공드리  장르-멜로/로맨스, 드라마, SF"))
-        items.add(ListViewItem(ContextCompat.getDrawable(this, R.drawable.snow_frozen)!!, "겨울왕국", "감독-크리스 벅, 제니퍼 리  장르-애니메이션, 모험, 코미디, 가족, 판타지, 뮤지컬"))
-        items.add(ListViewItem(ContextCompat.getDrawable(this, R.drawable.snow_loveletter)!!, "러브레터", "감독-이와이 슌지   장르-드라마, 멜로/로맨스"))
-        items.add(ListViewItem(ContextCompat.getDrawable(this, R.drawable.snow_train)!!, "설국열차", "감독-봉준호  장르-SF,액션,드라마"))
-        items.add(ListViewItem(ContextCompat.getDrawable(this, R.drawable.snow_love)!!, "러브 액츄얼리", "감독-리차드 커티스    장르-멜로/로맨스, 드라마, 코미디"))
-
-        val adapter: ListViewAdapter(items)
-        listView.adpater=adapter
-
-        listView.setOnItemClickListener { parent: AdapterView<*>, view: View, position: Int, id: Long ->
-            val item = parent.getItemAtPosition(position) as ListViewItem
-            Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
     }
 }
