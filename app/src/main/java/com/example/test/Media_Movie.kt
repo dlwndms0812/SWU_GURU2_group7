3ff56963b5f0e90ce7826a5903824512224a5c6f
cyeon1118 <cyeon1118@naver.com>
2021-02-09 17:29:46 +0900

    0209 17:28 수정

--- app/src/main/java/com/example/test/Media_Movie.kt
+++ app/src/main/java/com/example/test/Media_Movie.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class Media_Movie : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_media__movie)
+        setContentView(R.layout.activity_cloudy_movie)
     }
 }
